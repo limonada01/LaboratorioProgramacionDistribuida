98e3f11c662195fc6a3f1d04895b05cab19e072a
limon <daniel.campos@est.fi.uncoma.edu.ar>
2023-06-07 23:33:03 -0300

    solo tp4

--- RMI/Ejemplos/Ej1/Cliente/ServicioEco.java
+++
@@ -1,7 +0,0 @@
-package RMI.Ejemplos.Ej1.Cliente;// interface que contiene los métodos del servicio
-
-import java.rmi.*;
-
-interface ServicioEco extends Remote {
-        String eco (String s) throws RemoteException;
-}
--- TpLab2/servidor_horoscopo/Servicio_Horoscopo.java
+++
@@ -1,6 +0,0 @@
-package TpLab2.servidor_horoscopo;
-import java.rmi.*;
-
-public interface Servicio_Horoscopo extends Remote{
-    public String getHoroscopo (String signo) throws RemoteException; 
-}
